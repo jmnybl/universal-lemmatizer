17d6c6c05fabab8d7946ade6110f79be0e712fe6
Jenna Kanerva <jmnybl@utu.fi>
2018-04-19 10:22:50 +0300

    moving file

--- morpho_transducers/convert_all_transducers.sh
+++ morpho_transducers/convert_all_transducers.sh
@@ -11,5 +11,5 @@ for f in $input/* ; do
     echo "converting" $f
     base=$(basename $f)
 #    base=${base%.*}
-    zcat $f | python3 ../morpho_transducers/convert_to_ud.py -f apertium --feature_mapping ../morpho_transducers/apertium2ud.tsv | gzip -c > $output/$base
+    zcat $f | python3 convert_to_ud.py -f apertium --feature_mapping apertium2ud.tsv | gzip -c > $output/$base
 done
